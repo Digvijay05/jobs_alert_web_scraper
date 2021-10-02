e557c2075c5fefaaaeaa04ec13175b19c44805a3
Digvijay Jadav <digvijayjadav2404@gmail.com>
2021-10-02 13:38:44 +0530

    Modified tests.py by reducing non-required test #8

--- Posts/tests.py
+++ Posts/tests.py
@@ -3,10 +3,6 @@ from django.test import TestCase
 
 # Create your tests here.
 class MyTest(TestCase):
-    def test_worker(self):
-        response = self.client.post("/api/worker")
-        self.assertEqual(response.status_code, 200)
-
     def test_api_overview(self):
         response = self.client.get("/api/")
         self.assertEqual(response.status_code, 200)
