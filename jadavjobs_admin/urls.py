7c08ed79e76a414d7c319b9b909c5c84c5dcecf5
Digvijay Jadav <digvijayjadav2404@gmail.com>
2021-10-02 15:16:05 +0530

    Modified classes.py for efficiency #8

--- jadavjobs_admin/urls.py
+++ jadavjobs_admin/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import urls
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('rest-admin/', include(urls, namespace="rest_framework")),
                   path('api/', include("Posts.urls")),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
